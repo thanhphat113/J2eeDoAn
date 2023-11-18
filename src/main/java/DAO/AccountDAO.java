09123717fc269cffad30dd1080ce578cc65eda34
thivo1106 <thine0606@gmail.com>
2023-11-18 15:04:23 +0700

    fir

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,58 @@
+package DAO;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+
+import Database.DBContext;
+import java.util.ArrayList;
+import java.util.List;
+import model.Account;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import  java.sql.SQLException;
+
+/**
+ *
+ * @author thine
+ */
+public class AccountDAO extends DBContext{
+        
+      
+  
+    
+    public List<Account> getAll(){
+        List<Account> list = new ArrayList<>();
+        String sql = "select * from tblAccount";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                Account a = new Account(rs.getString("username"), rs.getString("password"));
+                list.add(a);
+            
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        AccountDAO a = new AccountDAO();
+        List<Account> list = a.getAll();
+        System.out.println(list.get(0).getUsername());
+        System.out.println(list.get(0).getPassword());
+      
+    }
+    
+    
+}
+
+
