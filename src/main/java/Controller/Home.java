54ac1b029c62a303224ee23f62fe84ea36ec08df
thanhphat113 <thanhphat9523@gmail.com>
2023-11-18 15:30:55 +0700

    2

--- src/main/java/Controller/Home.java
+++ src/main/java/Controller/Home.java
@@ -5,12 +5,15 @@
 
 package Controller;
 
+import DAO.sanphamDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.sanpham;
 
 /**
  *
@@ -38,7 +41,10 @@ public class Home extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException {
-        req.getRequestDispatcher("index.html").forward(req, resp);
+        sanphamDAO sp = new sanphamDAO();
+        List<sanpham> list = sp.findAll();
+        req.setAttribute("sanphamList", list);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
     } 
 
     @Override
